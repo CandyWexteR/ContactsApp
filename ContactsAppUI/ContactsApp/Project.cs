6a8188ee51d2a9d764a9730c76a2bd5ce10dc211
Алексей <mr.fostik@mail.ru>
2021-08-17 00:52:18 +0700

    Удалены лишние зависимости.

--- ContactsAppUI/ContactsApp/Project.cs
+++ ContactsAppUI/ContactsApp/Project.cs
@@ -1,9 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Linq.Expressions;
 using InvalidEditOperationException = ContactsApp.Exceptions.InvalidOperationException;
 
 namespace ContactsApp
