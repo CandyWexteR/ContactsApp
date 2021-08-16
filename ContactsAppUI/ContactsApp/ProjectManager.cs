9f6aefa724c86bbad861340b345848ebf6882c41
Алексей <mr.fostik@mail.ru>
2021-08-17 01:06:36 +0700

    Исправлен менеджер проекта.

--- ContactsAppUI/ContactsApp/ProjectManager.cs
+++ ContactsAppUI/ContactsApp/ProjectManager.cs
@@ -1,19 +1,32 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace ContactsApp
 {
     public class ProjectManager
     {
-        private Project _project = new Project();
+        private Project? _project;
+
+        //Для десериализации.
+        [System.Text.Json.Serialization.JsonConstructor]
+        public ProjectManager(Project project)
+        {
+            _project = project;
+        }
+
+        public int ContactsCount => _project.Contacts.Count;
 
         public void AddContact(Contact contact)
         {
             _project.AddContact(contact);
         }
 
-        public void SortContacts()
+        public IReadOnlyList<Contact> GetSortedContacts(Func<Contact, string> keySelector)
         {
-            _project.SortContactList();
+            return _project.Contacts.OrderBy(keySelector).ToList();
         }
 
         public void RemoveContact(int index)
@@ -21,52 +34,52 @@ namespace ContactsApp
             _project.RemoveContact(index);
         }
 
-        public int ContactsCount { get => _project.ContatsCount; }
-
-        public void EditContact(Contact before, Contact after)
+        public void EditContact(int indexBefore, Contact after)
         {
-            _project.EditContact(before, after);
+            _project.EditContact(indexBefore, after);
         }
 
         /// <summary>
         /// </summary>
         /// <param name="id"></param>
         /// <returns>Значение массива элементов типа Contact по его индексу</returns>
-        public Contact GetContact(int id) => _project.GetContact(id);
+        public Contact GetContact(int id) => _project.Contacts[id];
 
 
         public void Deserialize()
         {
-            Project project = null;
+            Project? project = null;
             //Создаём экземпляр сериализатора
-            JsonSerializer serializer = new JsonSerializer();
+            var serializer = new JsonSerializer();
             //Открываем поток для чтения из файла с указанием пути
-            using (StreamReader sr = new StreamReader(@"D:\ContactsAppVS\ContactsAppVS\contacts.json"))
-            using (JsonReader reader = new JsonTextReader(sr))
+            using (var sr = new StreamReader(@"D:\ContactsAppVS\ContactsAppVS\contacts.json"))
+            using (var reader = new JsonTextReader(sr))
             {
                 //Вызываем десериализацию и явно преобразуем результат в целевой тип данных
                 project = serializer.Deserialize<Project>(reader);
             }
 
-            _project.ContatsCount = project.ContatsCount;
-            if (project.ContatsCount != 0)
+            if (project != null)
             {
-                _project.contact = project.contact;
+                _project = project;
+            }
+            else
+            {
+                _project = new Project(new List<Contact>());
             }
         }
 
         public void Serialize()
         {
             //Создаём экземпляр сериализатора
-            JsonSerializer serializer = new JsonSerializer();
+            var serializer = new JsonSerializer();
             //Открываем поток для записи в файл с указанием пути
-            using (StreamWriter sw = new StreamWriter(@"D:\ContactsAppVS\ContactsAppVS\contacts.json"))
-            using (JsonWriter writer = new JsonTextWriter(sw))
+            using (var sw = new StreamWriter(@"D:\ContactsAppVS\ContactsAppVS\contacts.json"))
+            using (var writer = new JsonTextWriter(sw))
             {
                 //Вызываем сериализацию и передаем объект, который хотим сериализовать
                 serializer.Serialize(writer, _project);
             }
         }
-
     }
 }
