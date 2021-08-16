e888b8d4a65989fcb507127b9cd689764d264969
Алексей <mr.fostik@mail.ru>
2021-08-16 23:47:06 +0700

    Добавлено новое исключение.

---
+++ ContactsAppUI/ContactsApp/Exceptions/InvalidOperationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ContactsApp.Exceptions
+{
+    /// <summary>
+    /// Исключение, возникающее при условии отсутствия возможности проведения операции.
+    /// </summary>
+    public class InvalidOperationException : Exception
+    {
+        public InvalidOperationException(string message) : base(message)
+        {
+        }
+    }
+}
